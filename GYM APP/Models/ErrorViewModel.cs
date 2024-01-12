4e7c641a6319aa5067175a6f58764d0014236f9d
yahya-fk <fekyah0@gmail.com>
2024-01-13 00:27:08 +0100

    Add project files.

---
+++ GYM APP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GYM_APP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
