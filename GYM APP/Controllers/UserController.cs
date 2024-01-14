e232d85c5e427444ef882f811b7df56755829c9a
yahya-fk <fekyah0@gmail.com>
2024-01-14 21:52:03 +0100

    add user's log in and home

---
+++ GYM APP/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GYM_APP.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
